2e13de3a83af034304a71ba6213608640acce4d5
alexander1939 <alexander1939>
2024-02-22 22:43:29 -0600

    validaciones con el arduino

--- ejercicio3/controlador.py
+++ ejercicio3/controlador.py
@@ -1,21 +1,8 @@
-import sys
 import serial
 import time
-from crud import Crud
 
 class Controlador:
-    def __init__(self, estadoServo1, estadoServo2, servo1, servo2, barraEstado):
-        self.host = 'localhost'
-        self.user = 'root'
-        self.password = ''
-        self.database = 'arduino_bd'
-        self.crud = Crud(self.host, self.user, self.password, self.database)
-        self.result, self.error = self.crud.init_connection()
-
-        if not self.result:
-            print(f"Error al conectar a la base de datos: {self.error}")
-            sys.exit(1)
-
+    def __init__(self):
         self.arduino = serial.Serial('COM2', 9600, timeout=1)
         time.sleep(2)
         print(self.arduino)
@@ -28,64 +15,43 @@ class Controlador:
         if datos:
             print("Datos recibidos...")
             estados = eval(datos)
-            estadoServo1.set(str(estados[0]))
-            estadoServo2.set(estados[1])
-            servo2.set(estados[1])
+            self.estado_servo1 = str(estados[0])
+            self.estado_servo2 = estados[1]
+            self.servo2 = estados[1]
         else:
             print("No se recibieron datos...")
-            barraEstado.set("ERROR al conectar con Arduino")
+            self.barra_estado = "ERROR al conectar con Arduino"
+        self.posicion_actual_servo1 = int(self.estado_servo1)
+        self.posicion_actual_servo2 = int(self.estado_servo2)
 
-    def control_servo1(self, grados_servo1):
-        if grados_servo1 != "":
-            if grados_servo1.isdigit():
-                grados_servo1 = int(grados_servo1)
-                if 0 <= grados_servo1 <= 180:
-                    self.arduino.write(f"1:{grados_servo1}".encode('ascii'))
+    def control_servo1(self, grados):
+        if grados.isdigit():
+            grados = int(grados)
+            if 0 <= grados <= 180:
+                # Verificar si la nueva posición es diferente de la actual
+                if grados != self.posicion_actual_servo1:
+                    self.arduino.write(f"1:{grados}".encode('ascii'))
                     time.sleep(0.1)
-
-                    estadoS1 = self.arduino.readline().decode('utf-8')
-                    estadoServo1.set(estadoS1.strip())
-
-                    id_componente_servo1, error_componente_servo1 = self.crud.insert_componente(
-                        tipo='servo', nombre='Servo 1', descripcion=f'Descripcion del Servo 1 ({grados_servo1} grados)')
-
-                    if error_componente_servo1:
-                        return ('Error', f"Error al insertar componente del Servo 1 en la base de datos: {error_componente_servo1}", 'error')
-
-                    id_registro_servo1, error_registro_servo1 = self.crud.insert_registro(id_componente_servo1, grados_servo1)
-
-                    if error_registro_servo1:
-                        return ('Error', f"Error al insertar registro del Servo 1 en la base de datos: {error_registro_servo1}", 'error')
-                    else:
-                        return ('Éxito', f"Servo 1 registrado en la base de datos. ID del componente: {id_componente_servo1}, ID del registro: {id_registro_servo1}", 'info')
-
+                    estado_s1 = self.arduino.readline().decode('utf-8')
+                    # Actualizar la posición actual del servo1
+                    self.posicion_actual_servo1 = grados
+                    return estado_s1.strip()
                 else:
-                    return ('CUIDADO', 'El valor del Servo 1 debe estar entre 0 y 180', 'warning')
+                    return "NO SE HA CAMBIADO LA POSICIÓN DEL SERVO"
             else:
-                return ('Error', 'Solo valores numéricos para el Servo 1', 'error')
+                return "EL VALOR DEL SERVO DEBE ESTAR EN 0 A 180"
         else:
-            return ('Error', 'Introduce un valor para el Servo 1', 'error')
+            return "EL VALOR DEBE SER NUMERICO"
 
-    def control_servo2(self, grados_servo2):
-        self.arduino.write(f"2:{grados_servo2}".encode('ascii'))
-        time.sleep(0.1)
+    def control_servo2(self, grados):
+        if grados.isdigit():
+            grados = int(grados)
+            if 0 <= grados <= 180 and grados != self.posicion_actual_servo2:
+                self.arduino.write(f"2:{grados}".encode('ascii'))
+                time.sleep(0.1)
+                estado_s2 = self.arduino.readline().decode('utf-8')
+                # Actualizar la posición actual del servo2
+                self.posicion_actual_servo2 = grados
+                return estado_s2.strip()
 
-        estadoS2 = self.arduino.readline().decode('utf-8')
-        estadoServo2.set(estadoS2.strip())
-
-        try:
-            id_componente_servo2, error_componente_servo2 = self.crud.insert_componente(
-                tipo='servo', nombre='Servo 2', descripcion=f'Descripcion del Servo 2 ({grados_servo2} grados)')
-
-            if error_componente_servo2:
-                return ('Error', f"Error al insertar componente del Servo 2 en la base de datos: {error_componente_servo2}", 'error')
-
-            id_registro_servo2, error_registro_servo2 = self.crud.insert_registro(id_componente_servo2, grados_servo2)
-
-            if error_registro_servo2:
-                return ('Error', f"Error al insertar registro del Servo 2 en la base de datos: {error_registro_servo2}", 'error')
-            else:
-                return ('Éxito', f"Servo 2 registrado en la base de datos. ID del componente: {id_componente_servo2}, ID del registro: {id_registro_servo2}", 'info')
-
-        except Exception as e:
-            return ('Error', f"Error al interactuar con la base de datos: {e}", 'error')
+        return "NO SE HA CAMBIADO LA POSICIÓN DEL SERVO2"
