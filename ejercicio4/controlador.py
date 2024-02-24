b47114d4e84cbf3e581997be371f05e33660ed4d
Alexis1212asaxx <sanchezlopezfelixalexis1@.com>
2024-02-24 15:43:12 -0600

    vista

--- ejercicio4/controlador.py
+++ ejercicio4/controlador.py
@@ -2,30 +2,44 @@ import threading
 import serial
 import time
 from crud import Crud
+from ejercicio4.vista import Vista
 
 class Controlador:
     def __init__(self, vista):
-        host = 'localhost'
-        user = 'root'
-        password = ''
-        database = 'arduino_bd'
-        
         self.vista = vista
-        self.crud = Crud(host, user, password, database)
+        self.crud = Crud('', '', '', '')
         self.crud.init_connection()
-        self.estaCorriendo = False       
-        try:
-            self.arduino = serial.Serial('COM2', 9600, timeout=1)
-            time.sleep(2)
-            print(self.arduino)           
-            self.hiloSensores = threading.Thread(target=self.leer_sensores, daemon=True)
-            self.hiloSensores.start()
-        except Exception as e:
-            print("No se puede conectar con Arduino:", e)
-            self.vista.estadoTemp.set("No se puede conectar con Arduino")
-            self.vista.estadoLumin.set("No se puede conectar con Arduino")
-    
-    def leer_sensores(self):
+        self.estaCorriendo = True
+
+        self.hiloSensores = threading.Thread(target=self.leerSensores, daemon=True)
+        self.arduino = serial.Serial('COM2', 9600, timeout=1)
+        time.sleep(2)
+        print(self.arduino)
+
+        self.arduino.write("1:0".encode())
+        time.sleep(0.1)
+        datos = self.arduino.readline().decode('utf-8')
+        print(datos)
+        if datos:
+            print("Datos recibidos..")
+            estados = eval(datos)  # Convertir un String en una Tupla
+            self.vista.estadoTemp.set(estados[0])
+            self.vista.estadoLumin.set(estados[1])
+        else:
+            print("No se recibieron datos..")
+            self.vista.barraEstado.set("Error al conectar con Arduino")
+
+        self.vista.miVentana.protocol('WM_DELETE_WINDOW', self.finalizar)
+        self.hiloSensores.start()
+
+    def finalizar(self):
+        self.estaCorriendo = False
+        self.hiloSensores.join(0.1)
+        self.vista.miVentana.quit()
+        self.vista.miVentana.destroy()
+        self.arduino.close()
+
+    def leerSensores(self):
         while self.estaCorriendo:
             datos = self.arduino.readline().decode('utf-8').strip()
             if datos:
@@ -36,22 +50,9 @@ class Controlador:
                     self.vista.estadoTemp.set(valor)
                     self.vista.lstbox_temp.insert("end", valor)
                     self.vista.lstbox_temp.see("end")
-                    # Insertar registro en la base de datos
-                    idComponente = 1  # Suponiendo que el componente de temperatura tiene ID 1
-                    self.crud.insert_registro(idComponente, float(valor))
                 elif sensor == '2':
                     self.vista.estadoLumin.set(valor)
                     self.vista.lstbox_lumin.insert("end", valor)
                     self.vista.lstbox_lumin.see("end")
-                    # Insertar registro en la base de datos
-                    idComponente = 2  # Suponiendo que el componente de luminosidad tiene ID 2
-                    self.crud.insert_registro(idComponente, float(valor))
 
-    def __del__(self):
-        self.estaCorriendo = False
-        self.crud.close_connection()
-
-
-
-
-
+controlador = Controlador(Vista)
